df0d0e1d85fe89736b727b96cfeeb2565d8290ef
Zhanel1 <90245645+Zhanel1@users.noreply.github.com>
2021-10-11 03:21:29 +0600

    FWE

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
 var onScrollHandler = function() {
    // элемент который будем менять
    var yourImageElement = document.getElementById('content__1')
+   var title = document.getElementById('content__title')
+   var subtitle = document.getElementById('content__subtitle')
+   var text = document.getElementById('content__text')
+
    // позиция скролла
    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
  
    // проверка на определенных местах по пикселям..
-   if (scrollTop > 2801 && scrollTop < 2900) {
+   if (scrollTop > 1850 && scrollTop < 2373) {
      // меняем то что нужно
+     yourImageElement.src = './img/how/1.png'
+     subtitle.innerHTML = 'Create and sell your NFTs'
+     text.innerHTML = 'Once you’ve set up your wallet, connect it to NFTime by clicking the wallet icon.'
+   }
+   else if(scrollTop > 2373 && scrollTop < 2946){
      yourImageElement.src = './img/how/2.png'
+     subtitle.innerHTML = 'Create and sell your NFTs'
+     text.innerHTML = 'Set up your collection: Add links on your social media accounts, a description, profile and banner images.'
    }
-   else if(scrollTop <= 2900 ){
-      yourImageElement.src = './img/how/1.png'
-     
-   }
- 
+   else if(scrollTop > 2946 && scrollTop < 3519){
+     yourImageElement.src = './img/how/3.png'
+     subtitle.innerHTML = 'Create and sell your NFTs'
+     text.innerHTML = 'Upload your work (The platform supports images, videos, audios, or 3D arts), add description and title, and customize your NFTs.'
+  }
+  else if(scrollTop > 3519 && scrollTop < 4092){
+    yourImageElement.src = './img/how/4.png'
+    subtitle.innerHTML = 'Create and sell your NFTs'
+    text.innerHTML = 'Choose how you want to sell your NFTs: via auctions or fixed-price listings.'
+  }
+
+
    console.log(scrollTop)
  };
  
- var onScrollHandler2 = function() {
-  var yourImageElement = document.getElementById('content__1')
-  // позиция скролла
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-  // проверка на определенных местах по пикселям..
-  if (scrollTop > 2801 && scrollTop < 2900) {
-    // меняем то что нужно
-    yourImageElement.src = './img/how/2.png'
-  }
-  else if(scrollTop <= 2900 ){
-     yourImageElement.src = './img/how/1.png'
-    
-  }
-
-  console.log(scrollTop)
-};
- document.addEventListener ("scroll", this.onScrollHandler1);
- document.addEventListener ("scroll", this.onScrollHandler2);
+ document.addEventListener ("scroll", this.onScrollHandler);
