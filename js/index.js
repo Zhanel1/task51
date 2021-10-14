511b882a8538fd8130792f115544b24e6efdc356
Zhanel1 <90245645+Zhanel1@users.noreply.github.com>
2021-10-14 21:53:34 +0600

    Остался только адаптив

--- js/index.js
+++ js/index.js
@@ -2,43 +2,34 @@ var onScrollHandler = function() {
    // элемент который будем менять
    var yourImageElement = document.getElementById('content__1')
    var content1 = document.getElementById('item1')
+   bod
 
    // позиция скролла
    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
  
   //  console.log(scrollTop);
-   console.log(content1.offsetTop)
+   console.log(scrollTop)
    // проверка на определенных местах по пикселям..
    if (scrollTop > 2400 && scrollTop < 2946) {
      // меняем то что нужно
      yourImageElement.style.top = '0'
-     yourImageElement.style.width = '740px'
-     yourImageElement.style.height = '573px'
      yourImageElement.src = './img/how/1.png'
     }
    else if(scrollTop > 2946 && scrollTop < 3519){ 
     yourImageElement.style.top = '0'
-    yourImageElement.style.width = '740px'
-    yourImageElement.style.height = '573px'
     yourImageElement.src = './img/how/2.png'
     }
    else if(scrollTop > 3519 && scrollTop < 4092){
     yourImageElement.style.top = '0'
-    yourImageElement.style.width = '740px'
-    yourImageElement.style.height = '573px'
     yourImageElement.src = './img/how/3.png'
     }
     else if(scrollTop > 4092 && scrollTop < 4665){    
      yourImageElement.style.top = '0'
-     yourImageElement.style.width = '740px'
-     yourImageElement.style.height = '573px'
       yourImageElement.src = './img/how/4.png'
     }
     else if(scrollTop > 4665 && scrollTop < 1850){
       yourImageElement.style.position = 'auto'   
      yourImageElement.style.top = '0'
-     yourImageElement.style.width = '740px'
-     yourImageElement.style.height = '573px'
       console.log(yourImageElement.style.position)
     }
  };
